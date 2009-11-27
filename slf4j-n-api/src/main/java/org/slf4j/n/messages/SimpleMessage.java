1ee72713d3e56d61c0c61fe576f5b2521fdd239e
Joern Huxhorn <jhuxhorn@googlemail.com>
2009-11-27 07:55:30 +0100

    Added docs. Is now real bean.

--- slf4j-n-api/src/main/java/org/slf4j/n/messages/SimpleMessage.java
+++ slf4j-n-api/src/main/java/org/slf4j/n/messages/SimpleMessage.java
@@ -4,14 +4,38 @@ import org.slf4j.n.Message;
 
 import java.io.Serializable;
 
+/**
+ * The simplest possible implementation of Message. It just returns the String given as the c'tor argument.
+ *
+ * @author J&ouml;rn Huxhorn
+ */
 public class SimpleMessage
   implements Message, Serializable
 {
   private static final long serialVersionUID = -8398002534962715992L;
 
-  private final String message;
+  private String message;
+
+  public SimpleMessage()
+  {
+    this(null);
+  }
 
   public SimpleMessage(String message)
+  {
+    this.message = message;
+  }
+
+  /**
+   *
+   * @return the same as getFormattedMessage, this method exists to support xml serialization.
+   */
+  public String getMessage()
+  {
+    return message;
+  }
+
+  public void setMessage(String message)
   {
     this.message = message;
   }
