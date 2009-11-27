8d7f11920f1a0dfd22847fc901423cd0c03bbc29
Joern Huxhorn <jhuxhorn@googlemail.com>
2009-11-27 07:52:24 +0100

    Added test-dependency to sulky-junit.

---
+++ slf4j-n-api/src/test/java/org/slf4j/n/messages/SimpleMessageTest.java
@@ -0,0 +1,12 @@
+package org.slf4j.n.messages;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jhuxhorn
+ * Date: Nov 26, 2009
+ * Time: 11:08:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleMessageTest
+{
+}
